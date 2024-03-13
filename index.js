f58f239249aff46c0ef4f295e81616294ab955c4
yaaryash <yashwalke3434@gmail.com>
2024-03-13 20:14:04 +0530

    a

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+// const fs1 = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(fs1);
+// const fs3 = "yaaryash";
+// fs.writeFileSync("./txt/output.txt", fs3);
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("File written");
+      });
+    });
+  });
+});
